05bb6979bf1dd422e1fc82146cae0c525d331c34
angryMedea <tessieliu@icloud.com>
2024-09-13 10:49:52 +0100

    loading spinner

--- src/layouts/HomePage/components/Carousel.tsx
+++ src/layouts/HomePage/components/Carousel.tsx
@@ -1,7 +1,7 @@
 import { ReturnBook } from "./ReturnBook"
 import { useEffect, useState } from "react"
 import BookModel from "../../../models/BookModel"
-import { title } from "process"
+import { SpinnerLoading } from "../../Utils/SpinnerLoading"
 
 export const Carousel = () => {
 
@@ -69,9 +69,7 @@ export const Carousel = () => {
 
     if(isLoading){
         return (
-            <div className="container m-5">
-                <p>Loading..</p>
-            </div>
+            <SpinnerLoading/>
         )
     }
 
