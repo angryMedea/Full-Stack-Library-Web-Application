d8235fc747c4aedfe7f1e727a76185160561c615
angryMedea <tessieliu@icloud.com>
2024-09-27 14:43:46 +0100

    pagination bug fix for search and category

--- spring-boot-library/src/main/java/com/pageturner/spring_boot_library/dao/BookRepository.java
+++ spring-boot-library/src/main/java/com/pageturner/spring_boot_library/dao/BookRepository.java
@@ -6,10 +6,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.RequestParam;
 
+/**
+ * Spring Data REST will automatically generate RESTful APIs based on JpaRepository
+ * Automatic generation of CRUD operations as REST APIs
+ * e.g. automatic creation of RESTful search endpoints(like /search) for custom query methods
+ */
 public interface BookRepository extends JpaRepository<Book, Long> {
     Page<Book> findByTitleContaining(@RequestParam("title") String title, Pageable pageable);
 
     Page<Book> findByCategory(@RequestParam("category") String category, Pageable pageable);
-
 
 }
