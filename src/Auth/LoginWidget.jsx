eecc848664baac7897d52108f4cdbedc74ae8fa6
angryMedea <tessieliu@icloud.com>
2024-10-07 11:52:49 +0100

    develp signin widget

---
+++ src/Auth/LoginWidget.jsx
@@ -0,0 +1,28 @@
+import { Redirect } from "react-router-dom";
+import { useOktaAuth } from "@okta/okta-react";
+import OktaSignInWidget from "./OktaSigninWidget";
+// {} are part of js's obj destructuring syntax
+// this allows to extract specific properties from an obj
+const LoginWidget = ({ config }) => {
+    const { oktaAuth, authState } = useOktaAuth();
+    const onSuccess = (tokens) => {
+        oktaAuth.handleLoginRedirect(tokens);
+    }
+
+    const onError = (err) => {
+        console.log('Sign in error: ', err);
+    }
+
+    if (!authState) {
+        return (
+            <SpinnerLoading/>
+        );
+    }
+
+    return authState.isAuthenticated ?
+    <Redirect to={{ pathname: '/' }}/>
+    :
+    <OktaSignInWidget config = {config} onSuccess = {onSuccess} onError={onError}/>
+}
+
+export default LoginWidget
--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -3,6 +3,8 @@ export const oktaConfig = {
     issuer:'https://dev-87590633.okta.com/oauth2/default',
     redirectUri:'http://localhost:3000/login/callback',
     scopes:['openid','profile','email'],
+    // proof key for code exchange
     pkce: true,
+    // disable the check for https which can not secure the data transfers
     disableHttpsCheck: true,
 }
