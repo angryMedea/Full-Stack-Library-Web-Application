f443df6e5d81ba4b564a53d2ca9b8fc0488d9511
angryMedea <tessieliu@icloud.com>
2024-10-23 21:14:17 +0100

    returnBook update

--- spring-boot-library/src/main/java/com/pageturner/spring_boot_library/controller/BookController.java
+++ spring-boot-library/src/main/java/com/pageturner/spring_boot_library/controller/BookController.java
@@ -34,7 +34,6 @@ public class BookController {
         return bookService.currentLoanCount(userEmail);
     }
 
-
     @GetMapping("/secure/ischeckedout/byuser")
     public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token,
                                       @RequestParam Long bookId) {
@@ -49,5 +48,11 @@ public class BookController {
         return bookService.checkoutBook(userEmail, bookId);
     }
 
+    @PutMapping("/secure/return")
+    public void returnBook (@RequestHeader(value = "Authorization") String token,
+                              @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        bookService.checkoutBook(userEmail, bookId);
+    }
 
 }
