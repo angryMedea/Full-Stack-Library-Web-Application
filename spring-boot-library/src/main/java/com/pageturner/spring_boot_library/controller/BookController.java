4d26bf2e202120a05217f35c59de93b9fd900109
angryMedea <tessieliu@icloud.com>
2024-10-24 11:00:58 +0100

    renewLoan update

--- spring-boot-library/src/main/java/com/pageturner/spring_boot_library/controller/BookController.java
+++ spring-boot-library/src/main/java/com/pageturner/spring_boot_library/controller/BookController.java
@@ -52,7 +52,14 @@ public class BookController {
     public void returnBook (@RequestHeader(value = "Authorization") String token,
                               @RequestParam Long bookId) throws Exception {
         String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
-        bookService.checkoutBook(userEmail, bookId);
+        bookService.returnBook(userEmail, bookId);
+    }
+
+    @PutMapping("/secure/renew/loan")
+    public void renewLoan(@RequestHeader(value = "Authorization") String token,
+                          @RequestParam Long bookId) throws Exception {
+        String userEmail = ExtractJWT.payloadJWTExtraction(token, "\"sub\"");
+        bookService.renewLoan(userEmail, bookId);
     }
 
 }
