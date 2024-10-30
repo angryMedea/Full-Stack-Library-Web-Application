9a1af4c7e0a6a24e60f1dfaca0a8f59ba61ce679
angryMedea <tessieliu@icloud.com>
2024-10-30 10:30:48 +0000

    addBook component update

---
+++ spring-boot-library/src/main/java/com/pageturner/spring_boot_library/controller/AdminController.java
@@ -0,0 +1,31 @@
+package com.pageturner.spring_boot_library.controller;
+
+import com.pageturner.spring_boot_library.entity.Book;
+import com.pageturner.spring_boot_library.requestmodels.AddBookRequest;
+import com.pageturner.spring_boot_library.service.AdminService;
+import com.pageturner.spring_boot_library.utils.ExtractJWT;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:8080")
+@RestController
+@RequestMapping("api/admin")
+public class AdminController {
+    private AdminService adminService;
+
+//    @Autowired
+    public AdminController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
+    @PostMapping("/secure/add/book")
+    public void postBook(@RequestHeader(value="Authorization")String token,
+                         @RequestBody AddBookRequest addBookRequest) throws Exception {
+        String admin = ExtractJWT.payloadJWTExtraction(token,"\"userType\"");
+        if(admin == null || ! admin.equals("admin")){
+            throw new Exception("Administration page only");
+        }
+        adminService.postBook(addBookRequest);
+    }
+
+}
