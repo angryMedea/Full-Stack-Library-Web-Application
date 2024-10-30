adbae579fea6f5fa7c87ab34fd56510b697f4810
angryMedea <tessieliu@icloud.com>
2024-10-30 19:43:56 +0000

    deleteBook component update

--- spring-boot-library/src/main/java/com/pageturner/spring_boot_library/service/AdminService.java
+++ spring-boot-library/src/main/java/com/pageturner/spring_boot_library/service/AdminService.java
@@ -1,19 +1,53 @@
 package com.pageturner.spring_boot_library.service;
 
 import com.pageturner.spring_boot_library.dao.BookRepository;
+import com.pageturner.spring_boot_library.dao.CheckoutRepository;
+import com.pageturner.spring_boot_library.dao.ReviewRepository;
 import com.pageturner.spring_boot_library.entity.Book;
+import com.pageturner.spring_boot_library.entity.Checkout;
 import com.pageturner.spring_boot_library.requestmodels.AddBookRequest;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
 public class AdminService {
     private BookRepository bookRepository;
+    private ReviewRepository reviewRepository;
+    private CheckoutRepository checkoutRepository;
 
-    public AdminService(BookRepository bookRepository) {
+    public AdminService(BookRepository bookRepository, ReviewRepository reviewRepository, CheckoutRepository checkoutRepository) {
         this.bookRepository = bookRepository;
+        this.reviewRepository = reviewRepository;
+        this.checkoutRepository = checkoutRepository;
+    }
+
+    public void increaseBookQuantity(Long bookId) throws Exception{
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        if(!book.isPresent()) {
+            throw new Exception("Book not found");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() + 1);
+        book.get().setCopies(book.get().getCopies() + 1);
+
+        bookRepository.save(book.get());
+    }
+
+    public void decreaseBookQuantity(Long bookId) throws Exception{
+        Optional<Book> book = bookRepository.findById(bookId);
+
+        if(!book.isPresent() || book.get().getCopiesAvailable() <= 0 || book.get().getCopies() == 0) {
+            throw new Exception("Book not found or quantity locked");
+        }
+
+        book.get().setCopiesAvailable(book.get().getCopiesAvailable() - 1);
+        book.get().setCopies(book.get().getCopies() - 1);
+
+        bookRepository.save(book.get());
     }
 
     public void postBook(AddBookRequest addBookRequest) {
@@ -26,5 +60,16 @@ public class AdminService {
         book.setCategory(addBookRequest.getCategory());
         book.setImg(addBookRequest.getImg());
         bookRepository.save(book);
+    }
+
+    public void deleteBook(Long bookId) throws Exception{
+        Optional<Book> book = bookRepository.findById(bookId);
+        if(!book.isPresent()) {
+            throw new Exception("Book not found");
+        }
+
+        bookRepository.delete(book.get());
+        checkoutRepository.deleteById(bookId);
+        reviewRepository.deleteAllByBookId(bookId);
     }
 }
